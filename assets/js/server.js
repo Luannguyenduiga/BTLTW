210379a81b82b114c815e748eef578ee8247e93b
{LUANNGUYEN} <{nguyenthanhluan13022006@gmail.com}>
2026-04-02 00:13:03 +0700

    trang server

--- assets/js/server.js
+++ assets/js/server.js
@@ -12,7 +12,7 @@ const fastify = Fastify({ logger: true });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const IMAGE_ROOT = path.join(__dirname, '../images');
-
+// const FRONTEND_ROOT = path.join(__dirname,'../'); // Đường dẫn đến file index.html của bạn
 const start = async () => {
   try {
     // 1. Kết nối DB trước
@@ -36,6 +36,13 @@ const start = async () => {
       prefix: '/images/',
       list: false
     });
+
+    // await fastify.register(fastifyStatic, {
+    //   root: FRONTEND_ROOT,
+    //   prefix: '/', // Để trống để vào link là thấy web luôn
+    //   index: `index.html`,
+    //   decorateReply: false // Quan trọng: Fastify chỉ cho phép 1 cái decorate reply mặc định
+    // });
 
     // 3. Đăng ký Controller (Phải đăng ký TRƯỚC KHI listen)
     await fastify.register(productController);
